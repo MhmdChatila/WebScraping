0c6a143a960cfbb745a6aa36f3789c95f2f0a3f1
Mohammad Chatila <84202073+MhmdChatila@users.noreply.github.com>
2023-08-14 14:06:28 +0300

    new idea

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 after i was searching through the network dev tools, i found this:
 Deleted for being dynamic content
 im getting better use beautifulsoup and requests, but still at the very start
+
+Im trying to get the videos since the page is dynamically rendering content using javascript.
--- main.py
+++ main.py
@@ -1,33 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+from time import sleep
+from requests_html import HTMLSession
 
-URL = "https://ads.tiktok.com/business/creativecenter/inspiration/topads/pc/en?region=LB"
-headers= {"User-Agent":"Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)"}
-respones = requests.get(URL,headers=headers)
-soup = BeautifulSoup(respones.text,'lxml')
+URL = "https://ads.tiktok.com/business/creativecenter/inspiration/topads/pad/en?region=LB"
+s = HTMLSession()
+headers = {"User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)"}
 
-# Find elements that have any of the specified classes
-data = soup.find('div',class_=[
-    'CommonGridLayoutDataList_listWrap__aDyjD',
-    'index-mobile_listWrap__lcrSL',
-    'TopadsList_topadsDataContentWrap__bZ3dt',
-    'index-mobile_topadsDataContentWrap__4uruH',
-])
+response = s.get(URL, headers=headers)
+response.html.render(sleep=3)
 
-print(data)
+vids = response.html.xpath('//*[@id="ccContentContainer"]/div[2]/div/div[2]/div[1]/div[1]',first=True)
 
-target_classes2 = [
-    'CommonGridLayoutDataList_cardWrapper__jkA9g',
-    'TopadsList_cardWrapper__9A7Uf',
-    'index-mobile_cardWrapper__TEjKX',
-]
-if data:
-    nested_divs = data.find_all('div',class_=target_classes2)
-    for nested_div in nested_divs:
-        a_tags = nested_div.find_all('a')
-        for a_tag in a_tags:
-            href = a_tag.get("href")
-            if href:
-                print(href)
-else:
-    print("Parent div not found.")
+for items in vids.absolute_links:
+    print(items)
